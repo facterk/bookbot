66f464824e2b9ca17ac316fb0658fb30e8161614
facterk <155555881+facterk@users.noreply.github.com>
2025-04-15 07:24:48 +0300

    fixed an issue that was causing the program to not work properly due to an encoding error. The error was fixed by changing the encoding of the file to UTF-8.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def get_book_text(book_location):
-    with open(book_location) as f:
+    with open(book_location , encoding='utf-8') as f:
         file_contents = f.read()
     return file_contents
 def main():
