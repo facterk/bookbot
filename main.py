b5285d5e821076c6b78c13eea59e2ec85ee7befd
facterk <facterk6969@yahoo.com>
2025-04-19 02:25:45 +0330

    fixed the problem causing the program not to work correctly

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_book_text(book_location):
 def main():
     book_location = ("/home/karamalatia/workspace/github.com/bookbot/books/frankenstein.txt")
     book_text = get_book_text(book_location)
-    print(book_text)
+    return book_text
 def num_of_words():
     book_txt = main()
     book_txt_string = str(book_txt)
