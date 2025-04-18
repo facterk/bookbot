7a8d4d80e88f554c3ad55678e48ca39fa53e42af
facterk <facterk6969@yahoo.com>
2025-04-18 17:57:18 +0330

    added a function to find the number of words there are in the documant

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ def main():
     book_location = ("/home/karamalatia/workspace/github.com/bookbot/books/frankenstein.txt")
     book_text = get_book_text(book_location)
     print(book_text)
+def num_of_words():
+    word_Count = len(main.split())
+    result = (f"{word_count}words found in the documant")
+    return result
 main()
 
