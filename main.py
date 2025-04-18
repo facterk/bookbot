a2894a8cce4338502894c52341eab1e21c424fd4
facterk <facterk6969@yahoo.com>
2025-04-18 17:59:09 +0330

    fixed an issue causing the program not to work correctly

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     book_text = get_book_text(book_location)
     print(book_text)
 def num_of_words():
-    word_Count = len(main.split())
+    word_count = len(main.split())
     result = (f"{word_count}words found in the documant")
     return result
-main()
+num_of_words()
 
