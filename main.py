bc33ab2feee3485965ccb7b7cc0d687face53a98
facterk <facterk6969@yahoo.com>
2025-04-19 02:28:00 +0330

    fixed some spelling errors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def num_of_words():
     book_txt = main()
     book_txt_string = str(book_txt)
     word_count = len(book_txt_string.split())
-    result = f"{word_count} words found in the documant"
+    result = f"{word_count} words found in the document"
     return result
 print (num_of_words())
 
