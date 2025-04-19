b3e06d8f4360a066e27bd1e009f21230d84ddb31
facterk <facterk6969@yahoo.com>
2025-04-19 06:06:54 +0330

    moved some of the functions from main.py to stats.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 
-def get_book_text(book_location):
-    with open(book_location , encoding='utf-8') as f:
-        file_contents = f.read()
-    return file_contents
-def main():
-    book_location = ("/home/karamalatia/workspace/github.com/bookbot/books/frankenstein.txt")
-    book_text = get_book_text(book_location)
-    return book_text
 from stats import num_of_words
 print (num_of_words())
 
--- stats.py
+++ stats.py
@@ -1,4 +1,13 @@
-from main import main
+def get_book_text(book_location):
+    with open(book_location , encoding='utf-8') as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_location = ("/home/karamalatia/workspace/github.com/bookbot/books/frankenstein.txt")
+    book_text = get_book_text(book_location)
+    return book_text
+
 def num_of_words():
     book_txt = main()
     book_txt_string = str(book_txt)
