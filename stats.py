e9d21d0249b11342b2a4fe8ad8df156fb112b885
facterk <facterk6969@yahoo.com>
2025-04-20 11:42:08 +0330

    same as last message

--- stats.py
+++ stats.py
@@ -8,12 +8,9 @@ def get_book_text(book_location):
     return file_contents
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-    else:
-        book_location = ("books/frankenstein.txt")
-        book_text = get_book_text(book_location)
+    
+    book_location = ("books/frankenstein.txt")
+    book_text = get_book_text(book_location)
     return book_text
 
 def num_of_words():
